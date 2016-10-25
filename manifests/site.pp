8a11f9720902b35009c222727e57d7c9a7e8184a
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2016-10-25 15:53:38 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -48,7 +48,7 @@ node default {
   #   class { 'my_class': }
   include role::classroom
     exec { "message_of_the_day":
-   command  =>  ' cowsay "Welcome to my server" > /etc/motd',
+   command  =>  'cowsay "Welcome to my server" > /etc/motd',
    creates  =>  '/etc/motd',
    path     => '/usr/local/bin',
   }
