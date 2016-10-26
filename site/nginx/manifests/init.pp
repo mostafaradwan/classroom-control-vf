cea371ef11af0f5ec16d029228a4ec46af41813b
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2016-10-26 15:40:18 -0500

    Update init.pp
    Cosmetic changes

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,28 +1,28 @@
 #nginx/manifests/init.pp
 class nginx {
 
-  package { 'nginx':
+   package { 'nginx':
     ensure => installed,
-  }
+   }
 
-  file { '/var/www':
+   file { '/var/www':
     ensure => 'directory',
     group  => 'root',
     mode   => '0755',
     owner  => 'root',
     before => Package['nginx'],
-  }
+   }
 
-  file { '/var/www/index.html':
+   file { '/var/www/index.html':
     ensure => 'file',
     group  => 'root',
     mode   => '0755',
     owner  => 'root',
     source => 'puppet:///modules/nginx/index.html',
     before => File['/var/www'],
-  }
+   }
 
-  file { '/etc/nginx/nginx.conf':
+   file { '/etc/nginx/nginx.conf':
     ensure => 'file',
     group  => 'root',
     mode   => '0755',
@@ -30,9 +30,9 @@ class nginx {
     source => 'puppet:///modules/nginx/nginx.conf',
     before => Package['nginx'],
     #notify => Service['nginx'],
-  }
+   }
   
-  file { '/etc/nginx/conf.d/default.conf':
+   file { '/etc/nginx/conf.d/default.conf':
     ensure => 'file',
     group  => 'root',
     mode   => '0755',
@@ -40,12 +40,13 @@ class nginx {
     source => 'puppet:///modules/nginx/default.conf',
     before => Package['nginx'],
     #notify => Service['nginx'],
-  }
+   }
   
-  service { 'nginx':
+   service { 'nginx':
     ensure    => running,
     enable    => true,   
     subscribe => [ File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf'] ] ,
-}
-
+   }
+   
+   
 }
