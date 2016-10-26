3f8aebde0d1aa78a98af922c857849acac6eb050
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2016-10-26 16:35:29 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,7 +3,7 @@ class nginx {
 
    package { 'nginx':
     ensure => installed,
-    before => File ['/var/www','/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf']
+    before => File ['/var/www','/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf'] ,
    }
 
    file { '/var/www':
