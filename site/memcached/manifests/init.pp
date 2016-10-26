27b2279165242eb2f75f04be2e9b4e4e40e259f1
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2016-10-26 14:33:11 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,24 +1,22 @@
 #memcached/manifests/init.pp
 class memcached  { 
-
-package 'memcached' {
-  ensure  => 'present',
-
+  package 'memcached' {
+    ensure  => 'present',
 }
 
-file '/etc/sysconfig/memcached' {
-  ensure    =>  'file',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  source  => 'puppet:///modules/memcached/memcached',
-  require => Package['memcached'], 
-}
+  file '/etc/sysconfig/memcached' {
+    ensure    =>  'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/memcached/memcached',
+    require => Package['memcached'], 
+  }
 
-service { 'memcached':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/sysconfig/memcached'],
-}
+  service { 'memcached':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
 
 }
