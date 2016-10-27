711e6385be6f8908c9132348220958e4f4687de7
Mostafa Radwan <mostafa.radwan@appdynamics.com>
2016-10-27 10:04:52 -0500

    Update init.pp
    Making code simplifications and using variables

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,47 +1,40 @@
 #nginx/manifests/init.pp
 class nginx {
 
-   package { 'nginx':
-    ensure => installed,
-    #Added before here instead of require at each file resource that's dependent on the package
-    before => File['/var/www','/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf'] ,
+   File {
+      ensure => 'file',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
    }
 
-   file { '/var/www':
-    ensure   => 'directory',
-    group    => 'root',
-    mode     => '0755',
-    owner    => 'root',
+   package { 'nginx':
+    ensure => installed,
+    
+    #Added before here instead of require at each file resource that's dependent on the package
+    before => File['/var/www' ,'/var/www/index.html','/etc/nginx/nginx.conf'] ,
+   }
+
+   file {'/var/www' :
+    ensure   => directory,
     #require  => Package['nginx'],
    }
 
    file { '/var/www/index.html':
-    ensure   => 'file',
-    group    => 'root',
-    mode     => '0755',
-    owner    => 'root',
     source   => 'puppet:///modules/nginx/index.html',
     #require  => File['/var/www'],
    }
 
    file { '/etc/nginx/nginx.conf':
-    ensure   => 'file',
-    group    => 'root',
-    mode     => '0755',
-    owner    => 'root',
     source   => 'puppet:///modules/nginx/nginx.conf',
-    
+       
     #require  => Package['nginx'],
     #before => File['/etc/nginx/'],
     #notify => Service['nginx'],
    }
   
    file { '/etc/nginx/conf.d/default.conf':
-    ensure => 'file',
-    group  => 'root',
-    mode   => '0755',
-    owner  => 'root',
-    source => 'puppet:///modules/nginx/default.conf',
+     source => 'puppet:///modules/nginx/default.conf',
     #require  => Package['nginx'],
     #before => File['/etc/nginx/conf.d'],
     #notify => Service['nginx'],
@@ -53,7 +46,6 @@ class nginx {
     subscribe => [ File['/etc/nginx/nginx.conf'],File['/etc/nginx/conf.d/default.conf'] ] ,
    }
    
- 
   #Package['nginx'] -> File ['/var/www'] -> File ['/etc/nginx/nginx.conf'] ->  File ['/etc/nginx/conf.d/default.conf'] -> Service['nginx'] 
   
 }
